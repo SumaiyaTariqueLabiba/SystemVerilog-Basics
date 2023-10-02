05fb892897fc42270d4b45c597f6d700c65a7a4d
Sumaiya Tarique Labiba <labibatarique@gmail.com>
2023-10-02 08:48:07 +0600

    Delete Adder_design.v

--- Adder_design.v
+++
@@ -1,9 +0,0 @@
-module adder_design(input logic [7:0] a,
-input logic [7:0] b,
-input logic c_in,
-output logic [7:0] sum,
-output logic c_out);
-
-assign {c_out, sum}=a+b+c_in;
-
-endmodule: adder_design
